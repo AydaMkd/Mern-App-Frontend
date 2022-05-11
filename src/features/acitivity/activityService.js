ae074759ea1695e0c87cc2a07163b13f4f168156
MyMac <mymac@MyMacs-MBP.lan>
2022-05-11 15:01:12 -0500

    connecting to heroku

--- src/features/acitivity/activityService.js
+++ src/features/acitivity/activityService.js
@@ -2,7 +2,7 @@
  
 import axios from 'axios'
 
-const API_URL = "http://localhost:5000/api/activity/"
+const API_URL = "https://milestone-project2.herokuapp.com/activity/"
 
 
 // Create new activity/workout
--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,7 +1,7 @@
   
 import axios from 'axios'
 
-const API_URL = "http://localhost:5000/api/users/"
+const API_URL = "https://milestone-project2.herokuapp.com/users/"
 
 // Register user
 const register = async (userData) => {
