5638039c6a5eb7c08986d475b6d43c70025dfaea
MyMac <mymac@MyMacs-MBP.lan>
2022-05-11 15:36:01 -0500

    commit

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,7 +1,7 @@
   
 import axios from 'axios'
 
-const API_URL = "https://milestone-project2.herokuapp.com/routes/api/users/"
+const API_URL = "https://milestone-project2.herokuapp.com/api/users/"
 
 // Register user
 const register = async (userData) => {
